c61a2ef3fcc6e269ef85d58f61f067f5875ad5ea
aymhce <aymhce@gmail.com>
2026-07-09 23:19:02 +0200

    test fix

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import { defineConfig } from "vite";
 const buildVersion = String(Date.now());
 
 export default defineConfig({
-  // Important pour GitHub Pages (user site ET project site)
-  // et pour éviter les 404 + MIME text/html sur les modules.
-  base: "./",
+  base: "/personal-pwa/",
   define: {
     __APP_VERSION__: JSON.stringify(buildVersion)
   }
