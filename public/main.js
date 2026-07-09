e5ea5756219568d09d0d614b5c5fc1c3d1dc5e73
aymhce <aymhce@gmail.com>
2026-07-09 23:32:40 +0200

    test fix

---
+++ public/main.js
@@ -0,0 +1,8 @@
+const root = document.getElementById("app-root");
+
+if (root) {
+  root.innerHTML = `
+    <h1>Ma PWA Drive</h1>
+    <p>Le site est bien servi depuis <code>public/</code> via GitHub Pages.</p>
+  `;
+}
